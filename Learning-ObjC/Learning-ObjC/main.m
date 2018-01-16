0b2fad4d8a8cf51e9e50a90b3661d2969cbcc68c
SuperLazyDog <jyoitatsu@foxmail.com>
2018-01-16 23:15:25 +0900

    创建本仓库项目

---
+++ Learning-ObjC/Learning-ObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Learning-ObjC
+//
+//  Created by 徐伟达 on 2018/1/16.
+//  Copyright © 2018年 徐伟达. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+	return 0;
+}
