5aceea76b79bb79c30b30bf4c55468073af10c69
SuperLazyDog <jyoitatsu@foxmail.com>
2018-02-02 15:35:17 +0900

    Preparing: [准备第九章: 创建相关文件]

---
+++ Learning-ObjC/Learning-ObjC/Basic Review/S9 Foundationの重要なクラス/S9_Basic.m
@@ -0,0 +1,9 @@
+//
+//  S9_Basic.m
+//  Learning-ObjC
+//
+//  Created by 徐伟达 on 2018/2/2.
+//  Copyright © 2018年 徐伟达. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
---
+++ Learning-ObjC/Learning-ObjC/Basic Review/S9 Foundationの重要なクラス/S9_Header.h
@@ -0,0 +1,13 @@
+//
+//  S9_Header.h
+//  Learning-ObjC
+//
+//  Created by 徐伟达 on 2018/2/2.
+//  Copyright © 2018年 徐伟达. All rights reserved.
+//
+
+#ifndef S9_Header_h
+#define S9_Header_h
+
+
+#endif /* S9_Header_h */
